098daf5d78fefff4aca04ed51be1512867429a69
Javlon Sodikov <javlonuser@yahoo.com>
2016-01-27 18:18:17 +0500

    vcs

---
+++ public/classv.php
@@ -0,0 +1,16 @@
+<?php
+include "my\classi\myclass.php";
+use My\Classi as aaa ;
+/*class myc {
+
+    function __construct()
+    {
+        classi\myclass::hello();
+    }
+}
+$a = new myc();*/
+//my\classi\myclass::hello();
+//classi\myclass::hello();
+$a = new aaa\myclass2();
+//my\classi\myclass2::hello();
+//$a->hello();
