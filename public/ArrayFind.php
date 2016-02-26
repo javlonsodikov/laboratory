3623311719cd09d372d9e327f20c356099c03619
Javlon Sodikov <javlonuser@yahoo.com>
2016-02-26 16:17:06 +0500

    Initial commit

--- public/ArrayFind.php
+++ public/ArrayFind.php
@@ -1,23 +1,31 @@
 <?php
+/**
+ * @param $number
+ * @return mixed
+ */
 function find($number)
 {
-    $data = [1, 8, 16, 41, 76, 89, 91, 105, 106];
-//       0  1  2   3   4   5   6   7    8
+    $data = [1, 8, 16, 41, 76, 89, 91, 105, 106, 117, 119, 225, 546, 768, 897, 1000, 1100, 1540];
+//            0  1  2   3   4   5   6   7    8   9    10   11   12   13   14   15    16    17
 
     $lo = 0;
     $hi = count($data) - 1;
 
-    while ($lo <= $hi) {
-        //$hi = $lo + $hi/2;
-        $middle = (int)(floor($lo + $hi) / 2);
-        if ($data[$middle] > $number) {
-            $middle = ceil($middle / 2);
-        } elseif ($data[$middle] < $number) {
-            $middle = $middle + ceil($middle / 2);
+    while ($lo < $hi) {
+        $middle = (int)(round(($lo + $hi) / 2));
+        //echo "middle:" . $middle . PHP_EOL;
+        if ($data[$middle] < $number) {
+            $lo = $middle;
+            //echo "Lo:" . $middle . PHP_EOL;
+        } elseif ($data[$middle] > $number) {
+            $hi = $middle-1;
+            //echo "High:" . $middle . PHP_EOL;
         } else {
-            return $data[$middle];
+            //return $data[$middle];
+            return $middle;
         }
     }
+    return -1;
 }
 
-echo find(41);
+echo find(2000);
