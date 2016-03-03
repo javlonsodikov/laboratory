60073aac1ca8a12f6ff07cdd910a08a251439f1a
Javlon Sodikov <javlonuser@yahoo.com>
2016-03-03 11:19:41 +0500

    The Love-Letter Mystery

---
+++ public/TheLove-LetterMystery.php
@@ -0,0 +1,17 @@
+<?php
+<?php
+$_fp = fopen("php://stdin", "r");
+fscanf($_fp, "%d", $times);
+for($i=0;$i<$times;$i++){
+    fscanf($_fp, "%s", $str);
+    $left = substr($str, 0, floor(strlen($str) / 2));
+    $right = strrev(substr($str, ceil(strlen($str) / 2)));
+    $leftArr = str_split($left);
+    $rightArr = str_split($right);
+    $count=0;
+    for($o=0;$o<count($leftArr);$o++){
+        //echo ord($leftArr[$o]) . PHP_EOL;
+        $count+=abs(ord($leftArr[$o]) - ord($rightArr[$o]));
+    }
+    echo $count . PHP_EOL;
+}
